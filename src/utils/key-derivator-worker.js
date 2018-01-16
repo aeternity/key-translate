befc2966f8667a8a5854e201c7cb49c4aaa029f6
Dominik Schöni <dominik@dominikschoeni.ch>
2018-01-16 11:36:59 +0100

    feat(japanese): determines japanese language and uses appropriate wordlist

--- src/utils/key-derivator-worker.js
+++ src/utils/key-derivator-worker.js
@@ -8,6 +8,21 @@ const bip32pathPrefix = "m/0'/0/";
 const bip44pathPrefix = "m/44'/0'/0'/0/";
 const derivationTries = 100;
 
+import japaneseWordlist from 'bip39/wordlists/japanese.json';
+import englishWordlist from 'bip39/wordlists/english.json';
+
+function getWordlistBySecret(secret) {
+    let firstWord = secret.trim().split(/\u3000/g)[0] // use ideographic space in unicode
+
+    // check if it is a japanese secret
+    if (japaneseWordlist.indexOf(firstWord) !== -1) {
+        console.log('detected japanese language.')
+        return japaneseWordlist
+    }
+
+    return englishWordlist
+}
+
 self.onmessage = function(e) {
     if (e.data.msg === undefined) return;
 
@@ -19,8 +34,10 @@ self.onmessage = function(e) {
     let wif;
     let hdRoot;
 
+    let wordlist = getWordlistBySecret(secret)
+
     // check if we deal with a mnenomic phrase
-    if (bip39.validateMnemonic(secret) && bitcoinjs.address) {
+    if (bip39.validateMnemonic(secret, wordlist) && bitcoinjs.address) {
         hdRoot = bitcoinjs.HDNode.fromSeedBuffer(bip39.mnemonicToSeed(secret));
 
         // search for used address
